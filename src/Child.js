ab59af23870f404308561090a94f3ef4721c2eb6
hyeok <hyc7575@naver.com>
2017-03-04 17:03:08 +0900

    'react_study2'

---
+++ src/Child.js
@@ -0,0 +1,15 @@
+class Child extends React.Component {
+  render() {
+    const { name, phone, show, handleClick } = this.props;
+    return (
+      <li onClick={handleClick}>
+        <p>name: {name}</p>
+        <p style={{
+          display: show ? 'inline' : 'none'
+        }}>
+          {phone}
+        </p>
+      </li>
+    );
+  }
+}
