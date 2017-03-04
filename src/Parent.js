069ac172b5adc65c812df52c5d6e0e8fecbac44e
claire0130 <claire10040130@gmail.com>
2017-03-04 16:57:09 +0900

    add. 00_test

---
+++ src/Parent.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Child from './Child'; 
+
+class Parent extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			index: 0
+		};
+	}
+
+	handleClick(i) {
+		this.setState({
+			index: i
+		});
+	}
+
+  render () {
+    const { index } = this.state;
+    return (
+		<div>
+			<Child
+			isSelected={index === 0}
+			setIndex = {() => this.handleClick(0)}
+			name="gomugom" gender="male" />
+
+			<Child 
+			isSelected={index === 1}
+			setIndex = {() => this.handleClick(1)}
+			name="iu" gender="female" />
+
+			<Child 
+			isSelected={index === 2}
+			setIndex = {() => this.handleClick(2)}
+			name="iu2" gender="female" />
+		</div>
+    );
+  }
+}
+
+export default Parent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
