60d7bee050c350982a38807eeacbf1a40bdaa7f8
claire0130 <claire10040130@gmail.com>
2017-03-05 00:50:56 +0900

    Modify. Parent.js + Child.js

--- src/Parent.js
+++ src/Parent.js
@@ -2,58 +2,57 @@ import React from 'react';
 import Child from './Child'; 
 
 class Parent extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			index: 0
-		};
-	}
-
-	handleClick(i) {
-		this.setState({
-			index: i
-		});
-	}
-
-  render () {
-    const { index } = this.state;
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [{
+        name: 'gomugom',
+        phone: '010-1111-2222',
+        show: false
+      }, {
+        name: 'iu',
+        phone: '010-2222-3333',
+        show: false
+      }, {
+        name: 'akmu',
+        phone: '010-1133-3245',
+        show: false
+      }]
+    };
+  }
+  handleClick(i) {
+    console.log(this.state);
+    const newPeople = this.state.people;
+    newPeople[i].show = !newPeople[i].show;
+    this.setState({
+      people: newPeople
+    });
+  }
+  render() {
+    const people = this.state.people;
     return (
-		<div>
-			<Child
-			isSelected={index === 0}
-			setIndex = {() => this.handleClick(0)}
-			name="gomugom" gender="male" />
-
-			<Child 
-			isSelected={index === 1}
-			setIndex = {() => this.handleClick(1)}
-			name="iu" gender="female" />
-
-			<Child 
-			isSelected={index === 2}
-			setIndex = {() => this.handleClick(2)}
-			name="iu2" gender="female" />
-		</div>
+      <ul>
+        <Child
+          name={ people[0].name }
+          phone={ people[0].phone }
+          show={ people[0].show }
+          handleClick={this.handleClick.bind(this, 0)}
+        />
+        <Child
+          name={ people[1].name }
+          phone={ people[1].phone }
+          show={ people[1].show }
+          handleClick={this.handleClick.bind(this, 1)}
+        />
+        <Child
+          name={ people[2].name }
+          phone={ people[2].phone }
+          show={ people[2].show }
+          handleClick={this.handleClick.bind(this, 2)}
+        />
+      </ul>
     );
   }
 }
 
 export default Parent;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
