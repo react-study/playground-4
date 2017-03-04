60542cccc84f933ab4c127339e13471c8e7b6920
yum <yooomikim@naver.com>
2017-03-04 17:01:24 +0900

    리액트 테스트 2

--- src/Parent.js
+++ src/Parent.js
@@ -2,33 +2,55 @@ import React from 'react';
 import Child from './Child';
 
 class Parent extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      index : 0
+      people: [{
+        name: 'gomugom',
+        phone: '010-1111-2222',
+        show: false
+      }, {
+        name: 'iu',
+        phone: '010-2222-3333',
+        show: false
+      }, {
+        name: 'akmu',
+        phone: '010-1133-3245',
+        show: false
+      }]
     };
   }
   handleClick(i) {
+    console.log(this.state);
+    const newPeople = this.state.people;
+    newPeople[i].show = !newPeople[i].show;
     this.setState({
-      index: i
+      people: newPeople
     });
   }
-  render () {
-    const { index } = this.state;
+  render() {
+    const people = this.state.people;
     return (
-      <div>
+      <ul>
         <Child
-          isSelected = { index === 0 }
-          setIndex={() => this.handleClick(0)}
-          name="yum"
-          gender="female" />
+          name={ people[0].name }
+          phone={ people[0].phone }
+          show={ people[0].show }
+          handleClick={this.handleClick.bind(this, 0)}
+        />
         <Child
-          isSelected = { index === 1 }
-          setIndex={() => this.handleClick(1)}
-          name="yum2"
-          gender="female" />
-      </div>
-
+          name={ people[1].name }
+          phone={ people[1].phone }
+          show={ people[1].show }
+          handleClick={this.handleClick.bind(this, 1)}
+        />
+        <Child
+          name={ people[2].name }
+          phone={ people[2].phone }
+          show={ people[2].show }
+          handleClick={this.handleClick.bind(this, 2)}
+        />
+      </ul>
     );
   }
 }
