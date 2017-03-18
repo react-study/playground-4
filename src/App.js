57989d5f47db889f6c63074dd15c3f5a39182494
gomugom <power4ce@gmail.com>
2017-03-18 16:11:04 +0900

    투두 - 추가삭제

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {text: '배고파', id: 1000},
+                {text: '졸려', id: 1001},
+                {text: '날씨좋다', id: 1002}
+            ]
+        };
+    }
+    addTodo(text) {
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    text,
+                    id: Date.now()
+                }
+            ]
+        });
+    }
+    deleteTodo(id) {
+        const newTodos = [...this.state.todos];
+        const deleteIndex = newTodos.findIndex(v => v.id === id);
+        newTodos.splice(deleteIndex, 1);
+        this.setState({
+            todos: newTodos
+        });
+    }
+    render() {
+        return (
+            <div className="todo-app">
+                <Header
+                    addTodo={text => this.addTodo(text)}
+                />
+                <TodoList
+                    todos={this.state.todos}
+                    deleteTodo={id => this.deleteTodo(id)}
+                />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
