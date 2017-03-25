6b9205fea096361148725f45b7a418c4b9a7b2d8
gomugom <power4ce@gmail.com>
2017-03-25 17:03:01 +0900

    투두-ajax 추가 삭제

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 import TodoList from './TodoList';
 import Footer from './Footer';
 
+const ax = axios.create({
+    baseURL: 'http://localhost:2403/todos',
+    timeout: 1000
+});
+
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            todos: [
-                {
-                  text: '배고파',
-                  isDone: false,
-                  id: 1000
-                },
-                {
-                  text: '졸려',
-                  isDone: true,
-                  id: 1001
-                },
-                {
-                  text: '날씨좋다',
-                  isDone: false,
-                  id: 1002
-                }
-            ],
+            todos: [],
             editingId: null,
             filterName: 'All'
         };
     }
+
+    componentWillMount() {
+        ax.get('/').then(res => {
+            this.setState({
+                todos: res.data
+            });
+        });
+    }
     addTodo(text) {
-        this.setState({
-            todos: [
-                ...this.state.todos,
-                {
-                    text,
-                    isDone: false,
-                    id: Date.now()
-                }
-            ]
+        ax.post('/', { text }).then(res => {
+            this.setState({
+                todos: [ ...this.state.todos, res.data ]
+            });
         });
     }
     deleteTodo(id) {
-        const newTodos = [...this.state.todos];
-        const deleteIndex = newTodos.findIndex(v => v.id === id);
-        newTodos.splice(deleteIndex, 1);
-        this.setState({
-            todos: newTodos
+        ax.delete(`/${id}`).then(() => {
+            const newTodos = [...this.state.todos];
+            const deleteIndex = newTodos.findIndex(v => v.id === id);
+            newTodos.splice(deleteIndex, 1);
+            this.setState({
+                todos: newTodos
+            });
         });
     }
     editTodo(id) {
@@ -102,6 +97,7 @@ class App extends React.Component {
     }
 
     render() {
+        console.log('render');
         const {
             todos,
             editingId,
