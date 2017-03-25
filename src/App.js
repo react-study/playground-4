50c2bfa3ac5c896ceb5fd1e7015825486ddcff07
lim yuri <glassyiweb@gmail.com>
2017-03-25 15:17:04 +0900

    todo-toggle-toggleAll-deleteCompleted

--- src/App.js
+++ src/App.js
@@ -1,3 +1,128 @@
-/**
- * Created by limyuri on 2017. 3. 22..
- */
+
+import React from 'react';
+
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {
+                    text: '배고파',
+                    id: 1000,
+                    isDone: false
+                },
+                {
+                    text: '졸려',
+                    id: 1001,
+                    isDone: false
+                },
+                {
+                    text: '날씨좋다',
+                    id: 1002,
+                    isDone: false
+                }
+            ],
+            editingId: null
+        };
+    }
+    addTodo(text) {
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    text,
+                    id: Date.now(),
+                    isDone: false
+                }
+            ]
+        });
+    }
+    deleteTodo(id) {
+        const newTodos = [...this.state.todos];
+        const deleteIndex = newTodos.findIndex(v => v.id === id);
+        newTodos.splice(deleteIndex, 1);
+        this.setState({
+            todos: newTodos
+        });
+    }
+    editTodo(id) {
+        this.setState({
+            editingId: id
+        });
+    }
+    cancelEdit() {
+        this.setState({
+            editingId: null
+        });
+    }
+    saveTodo(id, newText) {
+        const newTodos = [ ...this.state.todos ];
+        const editIndex = newTodos.findIndex(v => v.id === id);
+        newTodos[editIndex] = Object.assign({}, newTodos[editIndex], {
+            text: newText
+        });
+        this.setState({
+            todos: newTodos,
+            editingId: null
+        });
+    }
+    toggleTodo(id){
+        const newTodos = [ ...this.state.todos ];
+        const toggleIndex = newTodos.findIndex(v => v.id === id);
+        newTodos[toggleIndex] = Object.assign({}, newTodos[toggleIndex], {
+            isDone : !newTodos[toggleIndex].isDone
+        });
+        this.setState({
+            todos : newTodos
+        })
+    }
+    toggleAll(){
+        const newToggleAll = !this.state.todos.every(v => v.isDone);
+        const newTodos = this.state.todos.map(todo => Object.assign({}, todo, {
+            isDone : newToggleAll
+        }));
+        this.setState({
+            todos: newTodos
+        });
+    }
+    deleteCompleted(){
+        const newTodos = this.state.todos.filter(todo => !todo.isDone);
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+    render() {
+        const {
+            todos,
+            editingId
+        } = this.state;
+
+        return (
+            <div className="todo-app">
+                <Header
+                    addTodo={text => this.addTodo(text)}
+                    toggleAll = {() => this.toggleAll()}
+                />
+                <TodoList
+                    todos      = {todos}
+                    editingId  = {editingId}
+                    deleteTodo = {id => this.deleteTodo(id)}
+                    editTodo   = {id => this.editTodo(id)}
+                    cancelEdit = {() => this.cancelEdit()}
+                    saveTodo   = {(id, newText) => this.saveTodo(id, newText)}
+                    toggleTodo = {id => this.toggleTodo(id)}
+                />
+                <Footer
+                    deleteCompleted = {() => this.deleteCompleted()}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,34 @@
-/**
- * Created by limyuri on 2017. 3. 22..
- */
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+            <span className="todo-count">
+                <strong>0</strong>{' '}
+                <span>items</span>{' '}
+                left
+            </span>
+                <ul className="todo-filters">
+                    <li>
+                        <a>All</a>
+                    </li>
+                    <li>
+                        <a>Active</a>
+                    </li>
+                    <li>
+                        <a>Completed</a>
+                    </li>
+                </ul>
+                <button
+                    className="todo-delete-completed"
+                    onClick={this.props.deleteCompleted}
+                >
+                    Clear Completed
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Footer;
