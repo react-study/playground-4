b913fe7795d9e1023747100ead3e94f47d314a43
lim yuri <glassyiweb@gmail.com>
2017-04-01 17:06:23 +0900

    router example

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import RouterMain from './routerExample/routerMain';
 
 ReactDOM.render(
-    <App/>,
+    <RouterMain/>,
     document.getElementById('root')
 )
---
+++ src/routerExample/routerMain.js
@@ -0,0 +1,40 @@
+/* main.js */
+import ReactDOM from 'react-dom';
+import React from 'react';
+import {
+    BrowserRouter as Router, // HashRouter, BrowserRouter 중 하나 사용.
+    Route,
+    Redirect,
+    Switch,
+    Link
+} from 'react-router-dom';
+import { Home, About, Name, Portfolio } from './Components';
+
+const routerMain = () => (
+    <Router>
+        <div>
+            <header>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/about/name">About - Name</Link></li>
+                    <li><Link to="/about/redirect">About - RedirectTo: Portfolio #1</Link></li>
+                    <li><Link to="/portfolio">Portfolio - All</Link></li>
+                    <li><Link to="/portfolio/0">Portfoilo - #0</Link></li>
+                    <li><Link to="/portfolio/1">Portfoilo - #1</Link></li>
+                </ul>
+            </header>
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/about/name" component={Name} />
+            <Switch>
+                <Redirect from="/about/redirect" to="/portfolio/1" />
+                <Route exact path="/portfolio" component={Portfolio} />
+                <Route path="/portfolio/:id" component={Portfolio} />
+            </Switch>
+        </div>
+    </Router>
+);
+
+export default routerMain;
