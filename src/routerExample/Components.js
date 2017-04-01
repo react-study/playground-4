1be57ec723f7c19f633037be3240c1362a9eb8d6
yum <yooomikim@naver.com>
2017-04-01 17:06:34 +0900

    라우터 예제

---
+++ src/routerExample/Components.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Home = () => <h2>HOME</h2>;
+
+export const About = () => ({ children }) => (
+  <div>
+    <h2>About</h2>
+    {children ? <div>{children}</div>: null}
+  </div>
+);
+
+export const Name = () => <h3>리액트스터디</h3>;
+
+const portfolios = [
+  { id: 0, text : 'portfolio #0'},
+  { id: 1, text : 'portfolio #1'},
+  { id: 2, text : 'portfolio #2'}
+];
+
+export const Portfolio = ({match}) => {
+  const filteredList = (match.params && match.params.id)
+    ? portfolios.filter(v => v.id === match.params.id)
+    : portfolios;
+    return (
+      <div>
+        <h2>portfolios</h2>
+        <ul>{filteredList.map(v => <li key={v.id}>{v.text}</li>)}</ul>
+      </div>
+    );
+};
