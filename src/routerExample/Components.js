d85db2a999377c65bb2d529f973dcd307d88126e
gomugom <power4ce@gmail.com>
2017-04-01 17:06:08 +0900

    라우터 예제

---
+++ src/routerExample/Components.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Home = () => <h2>HOME</h2>;
+
+export const About = () => <h2>About</h2>;
+
+export const Name = () => <h3>리액트스터디</h3>;
+
+const portfolios = [
+    { id: 0, text: 'portfolio #0' },
+    { id: 1, text: 'portfolio #1' },
+    { id: 2, text: 'portfolio #2' }
+];
+
+export const Portfolio = ({ match }) => {
+    console.log(match);
+    const filteredList = (match.params && match.params.id !== undefined)
+        ? portfolios.filter(v => v.id == match.params.id)
+        : portfolios;
+    return (
+        <div>
+            <h2>PORTFOLIOS</h2>
+            <ul>{filteredList.map(v => <li key={v.id}>{v.text}</li>)}</ul>
+        </div>
+    );
+};
