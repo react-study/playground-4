fccfa412af1e2b0257f2563eb3bb65c58aa59e3e
hyeok <hyc7575@naver.com>
2017-04-06 01:47:02 +0900

    'homework'

--- src/reduxExample/AccountBook.js
+++ src/reduxExample/AccountBook.js
@@ -1,31 +1,6 @@
 import React, {Component} from 'react';
 
 class AccountBook extends Component {
-    constructor(props) {
-        super(props);
-    }
-    AccountList() {
-        this.props.cashList.map((v,i) => {
-            console.log(v > 0)
-            if( v > 0 ) {
-                return `
-                    <tr>
-                        <td>${v}</td>
-                        <td></td>
-                        <td>${this.props.cash}</td>
-                    </tr>
-                `;
-            } else {
-                return `
-                    <tr>
-                        <td></td>
-                        <td>${Math.abs(v)}</td>
-                        <td>${this.props.cash}</td>
-                    </tr>
-                `;
-            }
-        });
-    }
     render() {
         return (
             <table style={{borderCollapse: 'collapse'}}>
@@ -38,24 +13,14 @@ class AccountBook extends Component {
                 </thead>
                 <tbody>
                     {
-                        this.props.cashList.map(({deposit, withdraw, cash},i) => {
-                            if( withdraw === '' ) {
-                                return (
-                                    <tr key={`data-row${i}`}>
-                                        <td>{deposit}</td>
-                                        <td></td>
-                                        <td>{cash}</td>
-                                    </tr>
-                                )
-                            } else {
-                                return (
-                                    <tr key={`data-row${i}`}>
-                                        <td></td>
-                                        <td>{Math.abs(withdraw)}</td>
-                                        <td>{cash}</td>
-                                    </tr>
-                                )
-                            }
+                        this.props.cashList.map(({deposit = '', withdraw = '', cash},i) => {
+                            return (
+                                <tr key={`data-row${i}`}>
+                                    <td>{deposit}</td>
+                                    <td>{withdraw ? Math.abs(withdraw) : ''}</td>
+                                    <td>{cash}</td>
+                                </tr>
+                            )
                         })
                     }
                 </tbody>
