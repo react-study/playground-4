b31df4091a40f901306b580ebfc08799661b9b20
gomugom <power4ce@gmail.com>
2017-04-08 17:07:59 +0900

    리덕스예제2 - 리덕스로 전환

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './reduxExample/App';
+import ReduxMain from './reduxExample/ReduxMain';
 
 ReactDOM.render(
-    <App/>,
+    <ReduxMain/>,
     document.getElementById('root')
 );
+
+/*
+//원래는 이렇게 들어오면 됩니다.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+ */
