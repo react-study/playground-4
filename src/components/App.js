290b8294cf9ffb75a8246c4e6a281ea893d0647e
gomugom <power4ce@gmail.com>
2017-04-15 17:04:50 +0900

    리덕스 실전 1

---
+++ src/components/App.js
@@ -0,0 +1,198 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+import TodoAction from '../actions/TodoAction';
+
+const mapStateToProps = state => ({
+    todos: state.todos,
+    editingId: state.editingId
+});
+const mapDispatchToProps = dispatch => ({
+    getTodos: () => dispatch(TodoAction.getTodos()),
+    addTodo: text => dispatch(TodoAction.addTodo(text)),
+    deleteTodo: id => dispatch(TodoAction.deleteTodo(id)),
+    editTodo: id => dispatch(TodoAction.editTodo(id)),
+    cancelEdit: () => dispatch(TodoAction.cancelEdit()),
+    saveTodo: (id, newText) => dispatch(TodoAction.saveTodo(id, newText)),
+    toggleTodo: id => dispatch(TodoAction.toggleTodo(id)),
+    toggleAll: () => dispatch(TodoAction.toggleAll()),
+    deleteCompleted: () => dispatch(TodoAction.deleteCompleted())
+});
+
+class App extends React.Component {
+    componentWillMount() {
+        this.props.getTodos();
+        /*
+        ax.get('/').then(res => {
+            this.setState({
+                todos: res.data
+            });
+        });
+        */
+    }
+    render() {
+        const {
+            todos,
+            editingId,
+            addTodo,
+            deleteTodo,
+            editTodo,
+            cancelEdit,
+            saveTodo,
+            toggleTodo,
+            toggleAll,
+            deleteCompleted,
+            match: {
+                params
+            }
+        } = this.props;
+
+        const filterName = params.filterName || '';
+
+        const filteredTodos = todos.filter(v => {
+            if (
+                !filterName ||
+                (filterName === 'active' && !v.isDone) ||
+                (filterName === 'completed' && v.isDone)
+            ) {
+                return true;
+            }
+        });
+        const activeLength = todos.filter(v => !v.isDone).length;
+
+        return (
+            <div className="todo-app">
+                <Header
+                    addTodo={addTodo}
+                    toggleAll={toggleAll}
+                />
+                <TodoList
+                    todos = {filteredTodos}
+                    editingId = {editingId}
+                    deleteTodo = {deleteTodo}
+                    editTodo = {editTodo}
+                    cancelEdit = {cancelEdit}
+                    saveTodo = {saveTodo}
+                    toggleTodo = {toggleTodo}
+                />
+                <Footer
+                    filterName = {filterName}
+                    activeLength = {activeLength}
+                    deleteCompleted = {deleteCompleted}
+                />
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+/*
+addTodo(text) {
+    ax.post('/', { text }).then(res => {
+        this.setState({
+            // todos: [ ...this.state.todos, res.data]
+            todos: update(this.state.todos, {
+                $push: [ res.data ]
+            })
+        });
+    });
+}
+deleteTodo(id) {
+    ax.delete(`/${id}`).then(() => {
+        this.setState(
+            update(this.state, {
+                todos: {
+                    $splice: [
+                        [ this.state.todos.findIndex(v => v.id === id), 1 ]
+                    ]
+                }
+            })
+        );
+    });
+}
+editTodo(id) {
+    this.setState({
+        editingId: id
+    });
+}
+cancelEdit() {
+    this.setState({
+        editingId: null
+    });
+}
+saveTodo(id, newText) {
+    const prevState = this.state;
+    const editIndex = prevState.todos.findIndex(v => v.id === id);
+    this.setState(
+        update(prevState, {
+            todos: {
+                [editIndex]: {
+                    text: {
+                        $set: newText
+                    }
+                }
+            },
+            editingId: {
+                $set: null
+            }
+        })
+    );
+    ax.put(`/${id}`, {
+        text: newText
+    }).catch(err => {
+        this.setState(prevState);
+    });
+}
+toggleTodo(id) {
+    const toggleIndex = this.state.todos.findIndex(v => v.id === id);
+    ax.put(`/${id}`, {
+        isDone: !this.state.todos[toggleIndex].isDone
+    }).then(res => {
+        this.setState(
+            update(this.state, {
+                todos: {
+                    [toggleIndex]: {
+                        $set: res.data
+                    }
+                }
+            })
+        );
+    });
+}
+toggleAll() {
+    const newToggleAll = !this.state.todos.every(v => v.isDone);
+    const axArray = this.state.todos.map(v =>
+        ax.put(`/${v.id}`, {
+            isDone: newToggleAll
+        })
+    );
+    axios.all(axArray).then(res => {
+        this.setState(
+            update(this.state, {
+                todos: {
+                    $apply: () => res.map(v => v.data)
+                }
+            })
+        );
+    });
+}
+deleteCompleted() {
+    const axArray = this.state.todos
+        .filter(todo => todo.isDone)
+        .map(v => ax.delete(`/${v.id}`));
+
+    axios.all(axArray).then(() => {
+        this.setState(
+            update(this.state, {
+                todos: {
+                    $apply: todos => todos.filter(v => !v.isDone)
+                }
+            })
+        );
+    });
+}
+*/
