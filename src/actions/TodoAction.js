440d2026e18c9f2df1009c340c348ae422d72177
yum <yooomikim@naver.com>
2017-04-22 15:18:23 +0900

    redux ajax 연결

---
+++ src/actions/TodoAction.js
@@ -0,0 +1,87 @@
+import axios from 'axios';
+
+const ax = axios.create({
+    baseURL: 'http://localhost:2403/todos',
+    timeout: 1000
+});
+
+const TodoAction = {
+  getTodos: () => dispatch => {
+      ax.get('/')
+      .then(res => {
+        dispatch({
+          type: 'GET_TODOS_RES',
+          todos: res.data
+        });
+    });
+  },
+  addTodo: text => dispatch => {
+    ax.post('/', { text })
+    .then(res => {
+      dispatch({
+        type: 'ADD_TODO',
+        newTodo: res.data
+      });
+    });
+  },
+  cancelEdit: id => ({
+    type: 'CANCEL_EDIT',
+    id
+  }),
+  deleteTodo: id => dispatch => {
+    ax.delete(`/${id}`)
+    .then(() => {
+      dispatch({
+        type: 'DELETE_TODO',
+        id
+      });
+    });
+  },
+  editTodo: id => ({
+    type: 'EDIT_TODO',
+    id
+  }),
+  saveTodo: (id, newText) => dispatch => {
+    ax.put(`/${id}`, { text: newText })
+    .then( res => {
+      dispatch({
+        type: 'SAVE_TODO',
+        id,
+        editedTodo: res.data
+      });
+    });
+  },
+  toggleTodo: (id, newDone) => dispatch => {
+    ax.put(`/${id}`, { isDone: newDone })
+    .then(res => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id,
+        toggledTodo: res.data
+      });
+    });
+  },
+  toggleAll: todos => dispatch => {
+    const newToggleAll = !todos.every(v => v.isDone);
+    const axArray = todos.map(todo=>
+      ax.put(`/${todo.id}`, { isDone : newToggleAll })
+    );
+    axios.all(axArray).then(res => {
+      dispatch({
+        type: 'TOGGLE_ALL',
+        toggledTodos: res.map(v => v.data)
+      });
+    });
+  },
+  deleteCompleted: todos => dispatch => {
+    const axArray = todos.filter(todo => todo.isDone)
+      .map(v => ax.delete(`/${v.id}`));
+    axios.all(axArray).then(() => {
+      dispatch({
+        type: 'DELETE_COMPLETED'
+      });
+    });
+  }
+};
+
+export default TodoAction;
---
+++ src/reducers/TodoReducer.js
@@ -0,0 +1,85 @@
+import update from 'immutability-helper';
+
+const initialState = {
+  todos: [],
+  editingId: null
+};
+
+const TodoReducer = ( state = initialState, action ) => {
+  switch(action.type) {
+    case 'GET_TODOS_RES': {
+      return update(state, {
+        todos: {
+          $set: action.todos
+        }
+      });
+    }
+    case 'ADD_TODO': {
+      return update(state, {
+        todos: {
+          $push: [ action.newTodo ]
+        }
+      });
+    }
+    case 'DELETE_TODO': {
+      return update(state, {
+        todos: {
+          $splice: [
+            [ state.todos.findIndex(v => v.id === action.id), 1 ]
+          ]
+        }
+      });
+    }
+    case 'EDIT_TODO': {
+      return update(state, {
+        editingId: {
+          $set: action.id
+        }
+      });
+    }
+    case 'CANCEL_EDIT': {
+      return update(state, {
+        editingId: {
+          $set: null
+        }
+      });
+    }
+    case 'SAVE_TODO': {
+      return update(state, {
+        todos: {
+          [state.todos.findIndex( v=> v.id === action.id)]: {
+            $set: action.editedTodo
+          }
+        },
+        editingId : {
+          $set: null
+        }
+      });
+    }
+    case 'TOGGLE_TODO': {
+      return update(state, {
+        todos: {
+          [state.todos.findIndex(v => v.id === action.id)]: {
+            $set: action.toggledTodo
+          }
+        }
+      });
+    }
+    case 'TOGGLE_ALL': {
+      return update(state, {
+        todos: {
+          $set : action.toggledTodos
+        }
+      });
+    }
+    case 'DELETE_COMPLETED': {
+      return update(state, {
+        todos: {
+          $set : state.todos.filter(v => !v.isDone)
+        }
+      });
+    }
+    default: return state;
+  }
+}
+export default TodoReducer;
